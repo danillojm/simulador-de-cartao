076d7dfeb941994d3525427f5b81cc69f0f22fdd
danillo.melo <danillo.melo@alclog.com.br>
2022-02-15 23:28:56 -0300

    ajustando o codigo do calculo e adicionando a tela home

--- src/app/component-form/component-form.component.ts
+++ src/app/component-form/component-form.component.ts
@@ -35,68 +35,34 @@ export class ComponentFormComponent implements OnInit {
 
   }
 
-  gerar() {
-
-    const conponentForm = {} as ComponentForm
-    conponentForm.vlrPurchase = this.formGroup.value.vlrPurchase
-    conponentForm.vlrInput = this.formGroup.value.vlrInput
-
-    this.calculatePercentage(this.formGroup.value.vlrPurchase)
-
-  }
-  /** Débito: 1,29%
-   Crédito á vista 2,30%
-   2x "3,42"%
-   3x "3,78"%
-   4x "4,15"%
-   5x 4,51%
-   6x 4,87%
-   7x 5,23%
-   8x 5,59%
-   9x 5,95%
-   10x 6,30%
-   11x 6,66%
-   12x 7,01%
-   2x 3,42%
-   3x 3,78%
-   4x 4,15%
-   5x 4,51%
-   6x 4,87%
-   7x 5,23%
-   8x 5,59%
-   9x 5,95%
-   10x 6,30%
-   11x 6,66%
-   12x 7,01%
-*/
-  calculatePercentage(vlrPurchase: number) {
+  calculation() {
     const percentage = ["1,29", "2,30", "3,42", "3,78", "4,15", "4,51", "4,87", "5,23", "5,59", "5,95", "6,30", "6,66", "7,1"]
     this.tableComponentValues = []
     let parcel = ''
     let vlrParcel = 0
     let totalValue = 0
     let vlrInput = this.formGroup.value.vlrInput
+    let vlrPurchase = this.formGroup.value.vlrPurchase
     vlrPurchase = vlrPurchase - vlrInput
 
     for (let i = 0; i < percentage.length; i++) {
 
-      const vlrPercentage: number = Number.parseFloat(percentage[i].replace(",", "."))
-      const vlrTotal = (vlrPercentage / 100) * vlrPurchase
 
-      console.log(vlrPurchase)
+      const taxa = Number.parseFloat(percentage[i].replace(",", "."))
+      const vlrPercentage = (taxa * vlrPurchase) / 100
+      const vlrTotal = vlrPurchase + vlrPercentage
 
       const componentTable = {} as ComponentTableModel
 
+      totalValue = vlrTotal
       if (i == 0) {
         parcel = "Débito"
-        vlrParcel = (vlrPurchase + vlrTotal)
+        vlrParcel = (vlrPurchase + vlrPercentage)
       } else {
 
         parcel = i + "x"
-        vlrParcel = (vlrPurchase + vlrTotal) / i
+        vlrParcel = vlrTotal / i
       }
-      totalValue = (vlrPurchase + vlrTotal)
-
 
       componentTable.parcel = parcel
       componentTable.vlrParcel = vlrParcel
